5b1eafc3eb00b4754ba18b914548c2c1e7c32d30
SharathW <sharathw@duck.com>
2025-05-02 12:30:28 +0800

    admin user creation one time

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ init_db()
 #creating admin user (run only once)
 from user_auth import init_user_db, add_user
 
-add_user("Admin", "sharath.sridhar@gmail.com", "ubuntu123", role="admin")
+add_user("Admin", "sharathw@duck.com", "admin", "ubuntu123")
 
 # Session state setup
 for key in ["is_authenticated", "user_name", "user_email", "user_role", "df", "emoji_rating", "rated_method", "feedback_type", "show_toast"]:
